d499b23b1e8523304f833cd2f810123bfcf3e0be
Esteban Boffa <esteban.boffa@mercadolibre.com>
2019-05-13 10:14:41 -0300

    change corner radius to 9

--- MyFirstApp/LoginViewController.swift
+++ MyFirstApp/LoginViewController.swift
@@ -17,7 +17,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        loginButton.layer.cornerRadius = 8
+        loginButton.layer.cornerRadius = 9
     }
     
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
